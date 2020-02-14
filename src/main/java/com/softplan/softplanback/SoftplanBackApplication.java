152232f84af2a533371c09bf61e50545a00f6d40
Gabriel Gorz <gabriel.gorz@cits.br>
2020-02-14 13:20:38 -0300

    First commit

---
+++ src/main/java/com/softplan/softplanback/SoftplanBackApplication.java
@@ -0,0 +1,13 @@
+package com.softplan.softplanback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftplanBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftplanBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softplan/softplanback/SoftplanBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softplan.softplanback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftplanBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
